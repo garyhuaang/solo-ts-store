c967b4cf3220bde04b9a15edcca1a5c200f1525d
Gary Huang <garyhuaang@gmail.com>
2025-04-04 14:12:09 -0500

    reordering branches

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// reorder comment for Git
+
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
