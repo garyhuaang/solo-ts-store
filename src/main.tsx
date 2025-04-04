cea7c23b2de09d8bd4d31eb79be98905cdcde26f
Gary Huang <garyhuaang@gmail.com>
2025-04-04 14:08:42 -0500

    reordering branches

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store.ts'
 
+// reorder comment in Git
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <App />
