b6b98b2508b2236e1cac308f7451598157c4eefd
Gary Huang <garyhuaang@gmail.com>
2025-04-04 11:34:04 -0500

    cleaned up Login css

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,9 @@
 import SubmitButton from '@/components/Buttons/SubmitButton'
 import { NavLogo } from '@/components/Navigation/InteractiveIcons'
 import { Button } from '@/components/ui/button'
-import { Card } from '@/components/ui/card'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
+import { loginUser } from '@/features/userSlice'
 import { useAppDispatch } from '@/hooks'
 import customFetch from '@/lib/utils/customFetch'
 import { ReduxStore } from '@/store'
@@ -25,23 +26,21 @@ export const action =
       return redirect('/')
     } catch (error) {
       console.log(error)
-      //   toast({ description: 'Login Failed' });
+
       return null
     }
   }
 
 function Login() {
-  const dispatch = useAppDispatch()
-
   return (
-    <div className="flex w-screen h-screen justify-center items-center">
-      <Card className="w-96 h-[30rem] place-content-center">
-        <Form className=" flex flex-col gap-4 p-6" method="POST">
-          <div className="flex flex-col justify-center  pb-4">
-            <NavLogo size="default" variant="ghost" />
-            <h1 className=" text-3xl text-center font-bold">Login</h1>
-          </div>
+    <div className="h-screen grid place-items-center">
+      <Card className="w-96 bg-muted">
+        <CardHeader>
+          <NavLogo size="default" variant="ghost" />
+          <CardTitle className="text-center">Login</CardTitle>
+        </CardHeader>
 
+        <Form method="POST">
           <Input type="email" placeholder="Email" />
           <Input type="password" placeholder="Password" />
 
@@ -50,12 +49,12 @@ function Login() {
             <Button variant="secondary">Guest User</Button>
           </div>
 
-          <div className="text-center text-sm pt-10">
+          <p className="text-center pt-10">
             Not a member yet?
             <Button asChild variant="link" size="lg">
               <Link to="/register">Reigster</Link>
             </Button>
-          </div>
+          </p>
         </Form>
       </Card>
     </div>
