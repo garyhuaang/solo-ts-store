197db68dd895f0de954f9663141c88f71b00ff66
Gary Huang <garyhuaang@gmail.com>
2025-04-04 13:43:38 -0500

    working

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,5 @@
-import { NavBar } from '@/components'
-import Header from '@/components/Header/Header'
-import { Outlet } from 'react-router-dom'
-
 function Home() {
-  return (
-    <div>
-      <Header />
-      <NavBar />
-      <Outlet />
-    </div>
-  )
+  return <div>Home</div>
 }
 
 export default Home
