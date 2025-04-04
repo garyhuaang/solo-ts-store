b7b9c46edfba00ef2d2c7ae50bab2cf730f3f491
Gary Huang <garyhuaang@gmail.com>
2025-04-04 14:00:11 -0500

    using older tailwindv3

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,5 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { About, Cart, Home, Login, Products, Register } from './pages'
-import Landing from './pages/Landing'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-    children: [
-      {
-        index: true,
-        element: <Landing />,
-      },
-      {
-        path: 'about',
-        element: <About />,
-      },
-      {
-        path: 'products',
-        element: <Products />,
-      },
-      {
-        path: 'cart',
-        element: <Cart />,
-      },
-    ],
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/register',
-    element: <Register />,
-  },
-])
-
 function App() {
-  return <RouterProvider router={router} />
+  return <div>App</div>
 }
 
 export default App
--- src/hooks.ts
+++
@@ -1,9 +0,0 @@
-import { RootApp, AppDispatch } from './store'
-import {
-  useDispatch,
-  useSelector,
-  type TypedUseSelectorHook,
-} from 'react-redux'
-
-export const useAppSelector: TypedUseSelectorHook<RootApp> = useSelector
-export const useAppDispatch: () => AppDispatch = useDispatch
