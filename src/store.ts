df44c6c865714d1ef84efca931a48b1a12f3e777
Gary Huang <garyhuaang@gmail.com>
2025-04-04 11:03:40 -0500

    Redux store type comments added

--- src/store.ts
+++ src/store.ts
@@ -12,6 +12,8 @@ export const store = configureStore({
 export type RootApp = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
+// we define this type explicitely for the store itself.
+// we want to work directly with the store object, not our defined component-level interactions
 export type ReduxStore = {
   getState: () => RootApp
   dispatch: AppDispatch
